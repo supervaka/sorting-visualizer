1a245fcbdea59ef8d675ed0226a70a74bd6f3d0f
Isaac Kim <isaac96kim@gmail.com>
2022-09-17 14:27:31 -0400

    staging all files

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h1>aoeu</h1>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Counter />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload. aoeuee
         </p>
         <span>
           <span>Learn </span>
