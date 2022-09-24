0fc038e4972d8f6fa33fbc4b5d9cc92e3c7f0af2
Isaac Kim <isaac96kim@gmail.com>
2022-09-24 14:31:15 -0400

    4 algos implemented

--- src/SortingVisualizer/SortingVisualizer.tsx
+++ src/SortingVisualizer/SortingVisualizer.tsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { getMergeSortAnimations } from "./sortingAlgorithms/sortingAlgorithms";
+import {
+  getMergeSortAnimations,
+  getQuickSortAnimations,
+  getHeapSortAnimations,
+  getInsertionSortAnimations,
+} from "./sortingAlgorithms/sortingAlgorithms";
 import "./SortingVisualizer.css";
 
 // Change this value for the speed of the animations.
@@ -14,7 +19,6 @@ const PRIMARY_COLOR = "turquoise";
 // This is the color of array bars that are being compared throughout the animations.
 const SECONDARY_COLOR = "red";
 
-// TODO: resolve temporary any
 export default class SortingVisualizer extends React.Component<any, any> {
   constructor(props: any) {
     super(props);
@@ -42,54 +46,85 @@ export default class SortingVisualizer extends React.Component<any, any> {
       const arrayBars = document.getElementsByClassName(
         "array-bar"
       ) as HTMLCollectionOf<HTMLElement>;
-      const isColorChange = i % 3 !== 2;
 
-      if (isColorChange) {
-        const [barOneIdx, barTwoIdx] = animations[i];
+      setTimeout(() => {
+        const [barOneIdx, newHeight] = animations[i];
         const barOneStyle = arrayBars[barOneIdx].style;
-        const barTwoStyle = arrayBars[barTwoIdx].style;
-        const color = i % 3 === 0 ? SECONDARY_COLOR : PRIMARY_COLOR;
-        setTimeout(() => {
-          barOneStyle.backgroundColor = color;
-          barTwoStyle.backgroundColor = color;
-        }, i * ANIMATION_SPEED_MS);
-      } else {
-        setTimeout(() => {
-          const [barOneIdx, newHeight] = animations[i];
-          const barOneStyle = arrayBars[barOneIdx].style;
-          barOneStyle.height = `${newHeight}px`;
-        }, i * ANIMATION_SPEED_MS);
-      }
+        barOneStyle.height = `${(newHeight / 10) * 0.9}%`;
+      }, i * ANIMATION_SPEED_MS * 5);
     }
   }
 
-  quickSort() {}
+  quickSort() {
+    const animations = getQuickSortAnimations(this.state.array);
+    for (let i = 0; i < animations.length; i++) {
+      const arrayBars = document.getElementsByClassName(
+        "array-bar"
+      ) as HTMLCollectionOf<HTMLElement>;
 
-  heapSort() {}
+      setTimeout(() => {
+        const [barOneIdx, newHeight] = animations[i];
+        const barOneStyle = arrayBars[barOneIdx].style;
+        barOneStyle.height = `${(newHeight / 10) * 0.9}%`;
+      }, i * ANIMATION_SPEED_MS);
+    }
+  }
 
-  insertionSort() {}
+  heapSort() {
+    const animations = getHeapSortAnimations(this.state.array);
+    for (let i = 0; i < animations.length; i++) {
+      const arrayBars = document.getElementsByClassName(
+        "array-bar"
+      ) as HTMLCollectionOf<HTMLElement>;
+
+      setTimeout(() => {
+        const [barOneIdx, newHeight] = animations[i];
+        const barOneStyle = arrayBars[barOneIdx].style;
+        barOneStyle.height = `${(newHeight / 10) * 0.9}%`;
+      }, i * ANIMATION_SPEED_MS * 3);
+    }
+  }
+
+  insertionSort() {
+    const animations = getInsertionSortAnimations(this.state.array);
+    for (let i = 0; i < animations.length; i++) {
+      const arrayBars = document.getElementsByClassName(
+        "array-bar"
+      ) as HTMLCollectionOf<HTMLElement>;
+
+      setTimeout(() => {
+        const [barOneIdx, newHeight] = animations[i];
+        const barOneStyle = arrayBars[barOneIdx].style;
+        barOneStyle.height = `${(newHeight / 10) * 0.9}%`;
+      }, i * ANIMATION_SPEED_MS);
+    }
+  }
 
   render() {
     const { array } = this.state;
 
     return (
-      <div className="array-container">
-        {array.map((value: number, idx: number) => (
-          <div
-            className="array-bar"
-            key={idx}
-            style={{
-              backgroundColor: PRIMARY_COLOR,
-              height: `${value}px`,
-            }}
-          ></div>
-        ))}
-        <button onClick={() => this.resetArray()}>Generate New Array</button>
-        <button onClick={() => this.mergeSort()}>Merge Sort</button>
-        <button onClick={() => this.quickSort()}>Quick Sort</button>
-        <button onClick={() => this.heapSort()}>Heap Sort</button>
-        <button onClick={() => this.insertionSort()}>Insertion Sort</button>
-      </div>
+      <>
+        <div className="array-container">
+          {array.map((value: number, idx: number) => (
+            <div
+              className="array-bar"
+              key={idx}
+              style={{
+                backgroundColor: PRIMARY_COLOR,
+                height: `${(value / 10) * 0.9}%`,
+              }}
+            ></div>
+          ))}
+        </div>
+        <div className="navbar">
+          <button onClick={() => this.resetArray()}>Generate New Array</button>
+          <button onClick={() => this.mergeSort()}>Merge Sort</button>
+          <button onClick={() => this.quickSort()}>Quick Sort</button>
+          <button onClick={() => this.heapSort()}>Heap Sort</button>
+          <button onClick={() => this.insertionSort()}>Insertion Sort</button>
+        </div>
+      </>
     );
   }
 }
